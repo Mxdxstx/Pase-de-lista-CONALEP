25ed6e7be78a6b5be17f46885a1d28dce172753f
GaelEsquivel <ruizesquivela@gmail.com>
2023-06-20 09:35:23 -0600

    Updated index x2 (pegué algo que no era)

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registrar usuario</title>
+	<title>Registrar Usuarios</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
-    <form method="post">
-    	<h1>¡Suscribete!</h1>
-    	<input type="text" name="name" placeholder="Nombre completo">
-    	<input type="email" name="email" placeholder="Email">
-    	<input type="submit" name="register">
-    </form>
-        <?php 
+	<form method="post">
+		<h1>Pase de Lista Conalep Juarez 1</h1>
+		<input type="text" name="name" placeholder="Nombre Completo">
+		<input type="email" name="email" placeholder="Email">
+		<input type="submit" name="register">
+	</form>
+		 <?php 
         include("registrar.php");
         ?>
 </body>
