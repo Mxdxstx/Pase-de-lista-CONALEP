531e6a813914d9a170ff74543621d33ab6e8e175
Mxdxstx <modestoivan19@gmail.com>
2023-07-29 16:38:01 -0600

    DataGrid, dropdowns and header UI

--- docentes.php
+++ docentes.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="css/docentes.css">
+<link rel="stylesheet" href="css/estilos.css">
 <?php
 session_start();
 if(empty($_SESSION["id"])) {
@@ -15,12 +17,68 @@ if(empty($_SESSION["id"])) {
     <title>Docentes</title>
 </head>
 <body>
-    <h1>DOCENTES</h1><br/>
-    <h2>
-    <?php 
-        echo $_SESSION["id_cargo"];
-        ?>
-    </h2>
-    <a href="controladores/controlador-cerrar-sesion.php">Salir</a>
+    <header>
+        <div class="date_time">
+
+        </div>
+        <div class="title_text">
+            <h1>DOCENTES</h1><br/>
+            <h2>Pase de lista</h2>
+        </div>
+        <div class="salir">
+            <a href="controladores/controlador-cerrar-sesion.php" class="btn_salir">Cerrar sesión</a>
+        </div>
+    </header>
+
+    <section class="main">
+        <div class="dropdowns">
+            <div class="semestres">
+
+                <label for="semestre">Semestre</label><br/>
+                <select name="semestre" id="semestre">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                </select>
+            </div>
+
+            <div class="grupos">
+            <label for="grupo">Elegir grupo</label><br/>
+                <select name="grupo" id="grupo">
+                    <option value="101-i">101-i</option>
+                    <option value="101-i">201-i</option>
+                    <option value="101-i">301-i</option>
+                </select>
+            </div>
+
+            <div class="asistentcia">
+                <p>Mostrar solo los que asistieron</p>
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
+            </div>
+        </div>
+
+        <div class="grid">
+            <table class="alumnos">
+                    <tr>
+                        <th>No.Control</th>
+                        <th>Nombre</th>
+                        <th>e-mail</th>
+                        <th>Fecha</th>
+                    </tr>
+                    <tr>
+                        <td>19111823</td>
+                        <td> Angel Ivan Modesto Hernandez</td>
+                        <td> modestoivan19@gmail.com</td>
+                        <td><?php echo  date('Y-m-d');?></td>
+                    </tr>
+            </table>
+        </div>
+    </section>
 </body>
 </html>
