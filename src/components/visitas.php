63b273cc139818e7a8aad39ffd4521610465dde2
Mxdxstx <modestoivan19@gmail.com>
2024-02-22 21:50:04 -0700

    Sección de registro de visitas
    Se creó la sección de visitas, su componente .php y su archivo de estilos .css

--- src/components/visitas.php
+++ src/components/visitas.php
@@ -1,36 +1,26 @@
-<?php
-session_start();
-if(empty($_SESSION["id"])) {
-    header("location: login.php");
-}
-include '../controllers/conexion.php';
-date_default_timezone_set('America/Chihuahua');
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio Prefectos</title>
 
-    <link rel="stylesheet" href="../css/inicioPrefectos.css">
     <link rel="stylesheet" href="../css/estilos.css">
     <link rel="stylesheet" href="../css/estilosReportes.css">
+    <link rel="stylesheet" href="../css/visitas.css">
 
     <script src="https://kit.fontawesome.com/41bcea2ae3.js" crossorigin="anonymous"></script>
+
+    <title>Registro de visitas</title>
 </head>
+<body>
+<header>
 
-<body id="body">
 
-    <header>
         <div class="icon__menu">
             <i class="fas fa-bars" id="btn_open"></i>
         </div>
         
-        <div class="main_text"><h1>Prefectura</h1></div>
+        <div class="main_text"><h1>Registro De Visitas</h1></div>
     </header>
 
     <div class="menu__side" id="menu_side">
@@ -42,7 +32,7 @@ date_default_timezone_set('America/Chihuahua');
 		
         <div class="options__menu">	
 			
-			<a href="inicioPrefectos.php" class="selected">
+			<a href="inicioPrefectos.php">
                 <div class="option">
                     <i class="fas fa-home" title="Inicio"></i>
                     <h4>Inicio</h4>
@@ -54,7 +44,7 @@ date_default_timezone_set('America/Chihuahua');
 					<h4>Pase De Lista</h4>
                 </div>
             </a>    
-            <a href="visitas.php" >
+            <a href="visitas.php" class="selected">
                 <div class="option">
                 <i class="fa-solid fa-book" ></i>
 					<h4>Registro De Visitas</h4>
@@ -91,15 +81,36 @@ date_default_timezone_set('America/Chihuahua');
     </div>
 
     <div class="main">
-        <br>
-		<h2>Bienvenido(a)</h2><br>
-		<div class="hora">
-            <p class="hour_text">La hora actual es:</p>
-            <p class='current_hour' id="horaActual"></p>
-        </div>
-    </div>
-	<script src="../scripts/prefectos/barralateral.js"></script>
-    <script src="../scripts/prefectos/horaActual.js"></script>
+        <form class="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+            Por favor llene los siguientes campos <br/> 
+            <input type="text" name="nombre" class="input_nombre" placeholder="Ingrese su nombre" /><br>
+            <input type="text" name="motivo" class="input_motivo" placeholder="Motivo de visita" /><br>
 
+            <button id="enviar" class="btn btn-enviar">
+                Enviar
+            </button> 
+        </form>
+
+        <table cellspacing="0" class="tabla_visitas">
+			<tr class="encabezados">
+                <th>Nombre</th>
+                <th>Asunto</th>
+                <th>Fecha y hora de entrada</th>
+            </tr>
+            <tr class="table_data">
+                <td>ANGEL IVÁN MODESTO HERNÁNDEZ</td>
+                <td>SERVICIO SOCIAL</td>
+                <td>2024-02-17 14:07:14</td>
+            </tr>
+            <tr class="table_data">
+                <td>GAEL ESQUIVEL</td>
+                <td>PROYECTO PASE DE LISTA</td>
+                <td>2024-01-17 09:22:22</td>
+            </tr>
+		</table>
+    </div>
+
+    <script src="../scripts/prefectos/barralateral.js"></script>
+    <script src="../scripts/prefectos/horaActual.js"></script>
 </body>
 </html>
