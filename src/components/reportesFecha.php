18f245f8d78fef8447c0904ad3f728999cb5c4c2
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-01-16 14:39:11 -0700

    Update reportesFecha.php

--- src/components/reportesFecha.php
+++ src/components/reportesFecha.php
@@ -31,8 +31,8 @@ $fecha = date("d-m-Y");
     <title>Prefectos</title>
 
 	<link rel="stylesheet" href="../css/reportes.css">	
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.25/jspdf.plugin.autotable.min.js"></script>
+    <script src="../scripts/prefectos/jspdf.umd.min.js"></script>
+    <script src="../scripts/prefectos/jspdf.plugin.autotable.min.js"></script>
 
 </head>
 
