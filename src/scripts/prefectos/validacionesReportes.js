629838e13520143f83c6c18fb5afbc36c8dba39a
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-01-21 19:51:26 -0700

    Add files via upload

---
+++ src/scripts/prefectos/validacionesReportes.js
@@ -0,0 +1,26 @@
+function validarFormulario(event) {
+    event.preventDefault();
+    const buscarInput = document.querySelector('input[name="buscar"]');
+    const valor = buscarInput.value.trim();
+
+    if (valor === "") {
+        alert("Por favor, ingrese una matrícula para buscar.");
+        return;
+    }
+
+    const matriculaRegex = /^[0-9-]+$/;
+    if (!matriculaRegex.test(valor)) {
+        alert("La matrícula solo debe contener números.");
+        return;
+    }
+
+    document.getElementById('formulario-busqueda').submit();
+
+    document.querySelector('input[name="buscar"]').addEventListener('input', function (event) {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+    
+    function recargarTabla() {
+        location.reload(); 
+    }
+}
