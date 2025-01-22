05fe4594950a03f781a26ffdb1120f1267461658
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-01-21 19:53:50 -0700

    Update registrar-asistencia.php

--- src/controllers/registrar-asistencia.php
+++ src/controllers/registrar-asistencia.php
@@ -6,7 +6,7 @@ $fechaHoraActual = date("Y-m-d H:i:s");
 
 if (isset($_POST['btnEnviar'])) {
     if (!empty($_POST["matriculaAuto"])) {
-        $codigo = str_replace("'", "-", $_POST["matriculaAuto"]);
+        $codigo = str_replace("'", "-", trim($_POST["matriculaAuto"]));
         $mtrValid = "SELECT * FROM alumnos WHERE matricula = '$codigo'";
         $mtrcons = mysqli_query($conexion, $mtrValid);
 
@@ -14,7 +14,6 @@ if (isset($_POST['btnEnviar'])) {
             $sql = "INSERT INTO asistencias (matricula, fecha_hora) VALUES ('$codigo', '$fechaHoraActual')";
             if ($conexion->query($sql) === TRUE) {
                 header("Location: " . $_SERVER['PHP_SELF']);   
-                echo "<p class='ok'>Se ingresó la matrícula correctamente.</p>";             
                 exit();
             } else {
                 echo "<p class='bad'>Error al insertar el registro: " . $conexion->error . "</p>";
