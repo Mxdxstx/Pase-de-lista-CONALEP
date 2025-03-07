cb57b70f3ff987890dd816cd80b4af5d5f4060be
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-03-06 20:29:28 -0700

    Update visitas.php

--- src/components/visitas.php
+++ src/components/visitas.php
@@ -15,7 +15,7 @@ $fecha = date("d-m-Y");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de visitas</title>
+    <title>Registro de Visitas</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <link rel="stylesheet" href="../css/estilos.css">
