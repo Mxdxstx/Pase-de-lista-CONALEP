700896968ba2c4ee754e092d8a6b1a2575541943
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-03-06 17:44:25 -0700

    Update docentes.php

--- src/components/docentes.php
+++ src/components/docentes.php
@@ -56,7 +56,6 @@ if (empty($_SESSION["id"])) {
                     ?>
                 </select>
             </div>
-            <br /><br />
             <div class="asistentcia">
                 <p>Mostrar solo los que <span style="text-decoration:underline">no</span> asistieron</p>
                 <label class="switch">
@@ -64,47 +63,18 @@ if (empty($_SESSION["id"])) {
                     <span class="slider round"></span>
                 </label>
             </div>
+            <br />
             <button id="exportarPDF" class="btn btn-success">Exportar Datos a PDF</button>
 
         </div>
 
         <div class="grid">
             <div id="tablaAlumnos">
-                <script>
-                    //TODO: cambiarlo a un script externo
-                    function cargarAlumnos() {
-                        var selectedGrupo = document.getElementById("grupo").value;
-
-                        var xhttp = new XMLHttpRequest();
-                        xhttp.onreadystatechange = function() {
-                            if (this.readyState == 4 && this.status == 200) {
-                                document.getElementById("tablaAlumnos").innerHTML = this.responseText;
-                            }
-                        };
-
-                        // Llamada inicial para cargar todos los alumnos al cargar la página
-                        xhttp.open("GET", "../controllers/cargar-alumnos.php?grupo=" + selectedGrupo, true);
-                        xhttp.send();
-
-                        const checkbox = document.getElementById('asistieron');
-
-                        checkbox.addEventListener('change', function() {
-                            // Verifica si el checkbox está marcado (activado)
-                            if (this.checked) {
-                                // Llamada para cargar solo los alumnos que no asistieron
-                                xhttp.open("GET", "../controllers/cargar-alumnos-ausentes.php?grupo=" + selectedGrupo, true);
-                                xhttp.send();
-                            } else {
-                                // Llamada para cargar todos los alumnos nuevamente
-                                xhttp.open("GET", "../controllers/cargar-alumnos.php?grupo=" + selectedGrupo, true);
-                                xhttp.send();
-                            }
-                        });
-                    }
-                </script>
             </div>
         </div>
         <script src="../scripts/docentes/exportarPDF.js"></script>
+        <script src="../scripts/docentes/CargarAlumnos.js"></script>
+
     </section>
 </body>
 </html>
