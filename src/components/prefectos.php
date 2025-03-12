41477e6fc2aacfa551788257b5abd371ddb142c2
GaelEsquivel <136777919+GaelEsquivel@users.noreply.github.com>
2025-03-12 13:51:00 -0600

    Update prefectos.php

--- src/components/prefectos.php
+++ src/components/prefectos.php
@@ -4,29 +4,7 @@ if (empty($_SESSION["id"])) {
 	header("location: login.php");
 }
 include '../controllers/conexion.php';
-
-$consulta = "SELECT * FROM usuarios";
-$guardar = $conexion->query($consulta);
-date_default_timezone_set('America/Mazatlan');
-$fecha = date("d-m-Y");
-
-$sql = "
-SELECT 
-    COUNT(asistencias.matricula) AS total_alumnos
-FROM asistencias
-INNER JOIN alumnos ON asistencias.matricula = alumnos.matricula
-INNER JOIN grupos ON alumnos.id_grupo = grupos.id_grupo
-WHERE DATE(fecha_hora) = CURDATE();
-";
-
-$resultado = $conexion->query($sql);
-
-$totalAlumnos = 0;
-
-if ($resultado->num_rows > 0) {
-    $fila = $resultado->fetch_assoc();
-    $totalAlumnos = $fila['total_alumnos'];
-}
+include '../controllers/contadorAlumnos.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
